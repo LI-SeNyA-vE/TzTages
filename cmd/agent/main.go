3443439b1c4c05953ec418c0084af3300cca3f78
LI-SeNyA-vE <arsikkor12@yandex.ru>
2025-03-31 11:52:23 +0300

    Вынес функции из main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -55,72 +55,90 @@ func main() {
 	0. Выйти
 > `)
 		choice, _ := reader.ReadString('\n')
-		switch strings.TrimSpace(choice) {
+		choice = strings.TrimSpace(choice)
+		switch choice {
 		case "1":
-			{
-				path, err := dialog.File().Title("Выбери изображение").Load()
-				path = strings.TrimSpace(path)
-				data, err := os.ReadFile(path)
-				if err != nil {
-					fmt.Println("Ошибка чтения файла:", err)
-					continue
-				}
-
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-				resp, err := client.UploadImage(ctx, &pb.UploadImageRequest{
-					FileName: filepath.Base(path),
-					Data:     data,
-				})
-				if err != nil {
-					fmt.Println("Ошибка загрузки:", err)
-					continue
-				}
-				fmt.Printf("Загружено: ID=%s, Размер=%d байт\n", resp.ImageMetadata.ImageId, resp.ImageMetadata.SizeBytes)
-			}
+			uploadImage(client)
 		case "2":
-			{
-				fmt.Print("Введите ID изображения: ")
-				id, _ := reader.ReadString('\n')
-				id = strings.TrimSpace(id)
-
-				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-				defer cancel()
-				resp, err := client.DownloadImage(ctx, &pb.DownloadImageRequest{ImageId: id})
-				if err != nil {
-					fmt.Println("Ошибка скачивания:", err)
-					continue
-				}
-
-				path, err := dialog.File().
-					Title("Сохранить файл").
-					SetStartFile(resp.ImageMetadata.FileName).
-					Save()
-
-				if err != nil {
-					fmt.Println("Возможно пользователь нажал Отмена", err)
-					continue
-				}
-				os.WriteFile(path, resp.Data, 0644)
-				fmt.Println("Изображение сохранено в файл:", resp.ImageMetadata.FileName)
-			}
-
+			downloadImage(client, reader)
 		case "3":
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
-			resp, err := client.ListImages(ctx, &pb.Empty{})
-			if err != nil {
-				fmt.Println("Ошибка получения списка:", err)
-				continue
-			}
-			for _, img := range resp.ImageMetadata {
-				fmt.Printf("ID: %s | Файл: %s | Размер: %d байт\n", img.ImageId, img.FileName, img.SizeBytes)
-			}
+			listImages(client)
 		case "0":
 			fmt.Println("Завершение.")
 			return
 		default:
 			fmt.Println("Неверный выбор.")
 		}
+	}
+}
+
+func uploadImage(client pb.ImageServiceClient) {
+	path, err := dialog.File().Title("Выбери изображение").Load()
+	if err != nil {
+		fmt.Println("Возможно пользователь нажал Отмена:", err)
+		return
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.UploadImage(ctx, &pb.UploadImageRequest{
+		FileName: filepath.Base(path),
+		Data:     data,
+	})
+	if err != nil {
+		fmt.Println("Ошибка загрузки:", err)
+		return
+	}
+	fmt.Printf("Загружено: ID=%s | Размер=%d байт\n", resp.ImageMetadata.ImageId, resp.ImageMetadata.SizeBytes)
+}
+
+func downloadImage(client pb.ImageServiceClient, reader *bufio.Reader) {
+	fmt.Print("Введите ID изображения: ")
+	id, _ := reader.ReadString('\n')
+	id = strings.TrimSpace(id)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.DownloadImage(ctx, &pb.DownloadImageRequest{ImageId: id})
+	if err != nil {
+		fmt.Println("Ошибка скачивания:", err)
+		return
+	}
+
+	path, err := dialog.File().Title("Сохранить файл").SetStartFile(resp.ImageMetadata.FileName).Save()
+	if err != nil {
+		fmt.Println("Отмена или ошибка сохранения:", err)
+		return
+	}
+
+	err = os.WriteFile(path, resp.Data, 0644)
+	if err != nil {
+		fmt.Println("Возможно пользователь нажал Отмена:", err)
+		return
+	}
+
+	fmt.Println("Изображение сохранено в хранилище:", path)
+}
+
+func listImages(client pb.ImageServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.ListImages(ctx, &pb.Empty{})
+	if err != nil {
+		fmt.Println("Ошибка получения списка:", err)
+		return
+	}
+
+	for _, img := range resp.ImageMetadata {
+		fmt.Printf("ID: %s | Файл: %s | Размер: %d байт\n", img.ImageId, img.FileName, img.SizeBytes)
 	}
 }
